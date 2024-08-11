0d339edfd5139d5ad1f7c7c10c53c8b08788bdef
sushant81074 <sushant94601@gmail.com>
2024-08-11 17:00:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const { json, urlencoded } = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+
+app.use(cors({ origin: "*" }));
+env.config({ path: "./secret.env" });
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  return res.status(200).send({ statusCode: 200, message: "home page" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`server running on port : ${process.env.PORT}`)
+);
