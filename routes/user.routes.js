2c7622ea22d925ba0331dff28ad9166e68763c23
sushantsharma-coco <cocosingh456@gmail.com>
2024-08-14 16:22:06 +0530

    middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,12 @@
-const { signIn } = require("../controllers/user.controller");
+const { signIn, signOut } = require("../controllers/user.controller");
+const { auth } = require("../middlewares/auth.middleware");
 
 const router = require("express").Router();
 
 router.route("/sign-in").post(signIn);
+
+router.use(auth);
+router.route("/sign-out").get(signOut);
 
 module.exports = {
   router,
